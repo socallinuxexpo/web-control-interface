b4804683a27802696b2c1fb0084144be63a93726
M Starch <lestarch@googlemail.com>
2015-02-06 20:24:18 -0800

    Addin in recording stubs

--- html-frontend/js/config.js
+++ html-frontend/js/config.js
@@ -3,7 +3,7 @@ CONFIG["url"] = "/control"
 CONFIG["config-url"] = CONFIG["url"] + "/config"
 CONFIG["pins-url"] = CONFIG["url"] + "/pins"
 CONFIG["rooms"] = {
-    "room1":"localhost",
-    "room2":"localhost",
-    "google":"google.com"
+    "room1":"http://localhost",
+    "room2":"http://localhost",
+    "google":"http://google.com"
 }
--- pybackend/config.py
+++ pybackend/config.py
@@ -10,17 +10,18 @@ CONFIG = {
         "Restart Signs" : {"url":"restart-signs","cmd" : "../bin/reset-signs","type":"button","args":[{"name":"room-url","label":"Room URL","hidden":True}]},
         "Start Stream"  : {"url":"start-stream" ,"cmd" : "../bin/start-stream","type":"select","args" : [{"name":"camera-url","label":"Camera URL"}]},
         "Stop Stream"   : {"url":"stop-stream"  ,"cmd" : "../bin/stop-stream","type":"button"},
-        "Wakeup X11"    : {"url":"wakeup-x11"   ,"cmd" : "../bin/wakeup","type":"button"}
+        "Start Recording":{"url":"record-start","cmd": "../bin/start-record","type":"buuton"},
+        "Stop Recording" :{"url":"record-start","cmd": "../bin/stop-record","type":"buuton"}
     },
     "PINS" : {
-        "Switch KVM" : {"url":"switch-kvm", "pin":1,"type":"button","args":[{"name":"cycle","hidden":True}]},
-        "Button 2"   : {"url":"button-2", "pin":2,"type":"button","args":[{"name":"cycle","hidden":True}]},
-        "Button 3"   : {"url":"button-3", "pin":3,"type":"button","args":[{"name":"cycle","hidden":True}]},
-        "Button 4"   : {"url":"button-4", "pin":4,"type":"button","args":[{"name":"cycle","hidden":True}]},
-        "Button 5"   : {"url":"button-5", "pin":5,"type":"button","args":[{"name":"cycle","hidden":True}]},
-        "Button 6"   : {"url":"button-6", "pin":6,"type":"button","args":[{"name":"cycle","hidden":True}]},
-        "Button 7"   : {"url":"button-7", "pin":7,"type":"button","args":[{"name":"cycle","hidden":True}]},
-        "KVM"        : {"url":"read-kvm", "pin":8,"type":"read"}
+        "Scan Left Adjust" : {"url":"scan-left", "pin":1,"type":"button","args":[{"name":"cycle","hidden":True}]},
+        "Scan Right Adjust": {"url":"scan-right","pin":2,"type":"button","args":[{"name":"cycle","hidden":True}]},
+        "Scan Down Adjust" : {"url":"scan-down", "pin":3,"type":"button","args":[{"name":"cycle","hidden":True}]},
+        "Scan Up Adjust"   : {"url":"scan-up",   "pin":4,"type":"button","args":[{"name":"cycle","hidden":True}]},
+        "Scan Menu"        : {"url":"scan-menu", "pin":5,"type":"button","args":[{"name":"cycle","hidden":True}]},
+        "Scan Zoom"        : {"url":"scan-zoom", "pin":6,"type":"button","args":[{"name":"cycle","hidden":True}]},
+        "KVM Switch Input" : {"url":"kvm-switch","pin":0,"type":"button","args":[{"name":"cycle","hidden":True}]},
+        "KVM Side"         : {"url":"kvm-read",  "pin":7,"type":"read"}
     } 
 }
 
