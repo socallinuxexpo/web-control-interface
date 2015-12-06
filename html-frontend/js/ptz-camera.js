214f9cd9b914b4c96410075033a119c51dc42c08
khaing211 <khaing211@gmail.com>
2015-12-05 17:51:20 -0800

    Rewrite ptz-camera.js with prototype based.

--- html-frontend/js/ptz-camera.js
+++ html-frontend/js/ptz-camera.js
@@ -1,75 +1,80 @@
 /**
- * @param Config interface
- * @return Camera interface
+ * Constructor for PTZOpticsCamera
  */
-function PTZOpticsCamera(config) {
+function PTZOpticsCamera(host, port, username, password, invertY) {
+  this.url = "http://" + host + ":" + port + "/cgi-bin/ptzctrl.cgi";
+  this.invertY = invertY;
+  this.username = username;
+  this.password = password;
+}
+
+PTZOpticsCamera.prototype.callPtzOpticsApi = function(query, successCallback, errorCallback) {
+  $.ajax({
+    type: "GET",
+    url: this.url + "?" + query,
+    username: this.username,
+    password: this.password,
+    success: successCallback,
+    error: errorCallback
+  });
+}
   
-  this.config = config;
-  this.zoomLevel = config["initial-zoom-level"] ? config["initial-zoom-level"] : 0;
-  
-  function callPtzOpticsApi(query, successCallback, errorCallback) {
-    $.ajax({
-      type: "GET",
-      url: config["url"] + "?" + query,
-      username: "admin",
-      password: "sCalAV13",
-      success: successCallback,
-      error: errorCallback
-    });
+PTZOpticsCamera.prototype.up = function(tiltSpeed, successCallback, errorCallback) {
+  if (this.invertY) {
+    var query = "ptzcmd&down&1&" + tiltSpeed;
+  } else {
+    var query = "ptzcmd&up&1&" + tiltSpeed;
   }
   
-  return {
-    up: function(tiltSpeed, successCallback, errorCallback) {
-      if (config.invertControl) {
-        var query = "ptzcmd&down&1&" + tiltSpeed;
-      } else {
-        var query = "ptzcmd&up&1&" + tiltSpeed;
-      }
-      
-      callPtzOpticsApi(query, successCallback, errorCallback);
-    },
-    down: function(tiltSpeed, successCallback, errorCallback) {
-      if (config.invertControl) {
-        var query = "ptzcmd&up&1&" + tiltSpeed;
-      } else {
-        var query = "ptzcmd&down&1&" + tiltSpeed;
-      }
-      
-      callPtzOpticsApi(query, successCallback, errorCallback);
-    },
-    left: function(panSpeed, successCallback, errorCallback) {
-      if (config.invertControl) {
-        var query = "ptzcmd&right&" + panSpeed + "&1";
-      } else {
-        var query = "ptzcmd&left&" + panSpeed + "&1";
-      }
-      
-      callPtzOpticsApi(query, successCallback, errorCallback);
-    },
-    right: function(panSpeed, successCallback, errorCallback) {
-      if (config.invertControl) {
-        var query = "ptzcmd&left&" + panSpeed + "&1";
-      } else {
-        var query = "ptzcmd&right&" + panSpeed + "&1";
-      }
-      
-      callPtzOpticsApi(query, successCallback, errorCallback);
-    },
-    stop: function(successCallback, errorCallback) {
-      var query = "ptzcmd&ptzstop&1&1";
-      callPtzOpticsApi(query, successCallback, errorCallback);
-    },
-    zoomIn: function(zoomSpeed, successCallback, errorCallback) {
-      var query = "ptzcmd&zoomin&" + zoomSpeed;
-      callPtzOpticsApi(query, successCallback, errorCallback);
-    },
-    zoomOut: function(zoomSpeed, successCallback, errorCallback) {
-      var query = "ptzcmd&zoomout&" + zoomSpeed;
-      callPtzOpticsApi(query, successCallback, errorCallback);
-    },
-    zoomStop: function(successCallback, errorCallback) {
-      var query = "ptzcmd&zoomstop&0";
-      callPtzOpticsApi(query, successCallback, errorCallback);
-    }
-  };
+  this.callPtzOpticsApi(query, successCallback, errorCallback);
+}
+
+PTZOpticsCamera.prototype.down = function(tiltSpeed, successCallback, errorCallback) {
+  if (this.invertY) {
+    var query = "ptzcmd&up&1&" + tiltSpeed;
+  } else {
+    var query = "ptzcmd&down&1&" + tiltSpeed;
+  }
+  
+  this.callPtzOpticsApi(query, successCallback, errorCallback);
+}
+
+PTZOpticsCamera.prototype.left = function(panSpeed, successCallback, errorCallback) {
+  if (this.invertY) {
+    var query = "ptzcmd&right&" + panSpeed + "&1";
+  } else {
+    var query = "ptzcmd&left&" + panSpeed + "&1";
+  }
+  
+  this.callPtzOpticsApi(query, successCallback, errorCallback);
+}
+
+PTZOpticsCamera.prototype.right = function(panSpeed, successCallback, errorCallback) {
+  if (this.invertY) {
+    var query = "ptzcmd&left&" + panSpeed + "&1";
+  } else {
+    var query = "ptzcmd&right&" + panSpeed + "&1";
+  }
+  
+  this.callPtzOpticsApi(query, successCallback, errorCallback);
+}
+
+PTZOpticsCamera.prototype.stop: function(successCallback, errorCallback) {
+  var query = "ptzcmd&ptzstop&1&1";
+  this.callPtzOpticsApi(query, successCallback, errorCallback);
+}
+
+PTZOpticsCamera.prototype.zoomIn: function(zoomSpeed, successCallback, errorCallback) {
+  var query = "ptzcmd&zoomin&" + zoomSpeed;
+  this.callPtzOpticsApi(query, successCallback, errorCallback);
+}
+
+PTZOpticsCamera.prototype.zoomOut: function(zoomSpeed, successCallback, errorCallback) {
+  var query = "ptzcmd&zoomout&" + zoomSpeed;
+  this.callPtzOpticsApi(query, successCallback, errorCallback);
+}
+
+PTZOpticsCamera.prototype.zoomStop: function(successCallback, errorCallback) {
+  var query = "ptzcmd&zoomstop&0";
+  this.callPtzOpticsApi(query, successCallback, errorCallback);
 }
