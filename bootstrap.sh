e9a4dc44236687f3a317ae75538685dee366e240
Derek Nowicki <derek@knowikiapps.com>
2017-02-19 10:40:22 -0800

    derekn: started over using ubuntu precise64. not working yet, but close

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-apt-get install -y python-dev build-essential
-apt-get install -y python3-dev
-apt-get install -y python3-pip python-pip
-apt-get install -y uwsgi-plugin-python3
+#install
+pip install uwsgi flask flask-restful
 
-pip install --user uwsgi flask flask-restful
+# create some symbolic links for some stuff
+# This should probably be moved to the vagrant file as a copy operation,
+# or even set up a sync situation.
+sudo ln -s /http/web-control-interface/system-configs/web-ctrl /etc/nginx/sites-enabled/web-ctrl
+sudo ln -s /http/web-control-interface/system-configs/web-ctrl.conf /etc/init/web-ctrl.conf
 
-mkdir /var/run/uwsgi
-#chown user:user /var/run/uwsgi
-chmod 755 /var/run/uwsgi
-chmod 777 /var/log/uwsgi
+# TODO: What does this do?
+sudo uwsgi --socket /var/run/uwsgi/uwsgi.sock --plugin /usr/lib/uwsgi/plugins/python3 web:app
 
-ln -s /http/web-control-interface/system-configs/web-ctrl /etc/nginx/sites-enabled/web-ctrl
-ln -s /http/web-control-interface/system-configs/web-ctrl.conf /etc/init/web-ctrl.conf
-mv /etc/X11/xinit/xinitrc /etc/X11/xinit/xinitrc.bak
-ln -s /http/web-control-interface/system-configs/xinitrc /etc/X11/xinit/xinitrc
-
-systemctl start nginx
-uwsgi --socket /var/run/uwsgi/uwsgi.sock --plugin /usr/lib/uwsgi/plugins/python3 -w web:app
+# start nginx service
+service nginx start
