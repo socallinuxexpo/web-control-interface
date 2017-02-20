04895f1131080deef1f982261aecff786602ea23
aclorta <aclorta@gmail.com>
2017-02-19 19:40:06 -0800

    aclorta: send uwsgi to background on launch

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,10 @@ pip install uwsgi flask flask-restful
 sudo ln -s /http/web-control-interface/system-configs/web-ctrl /etc/nginx/sites-enabled/web-ctrl
 sudo ln -s /http/web-control-interface/system-configs/web-ctrl.conf /etc/init/web-ctrl.conf
 
-# TODO: What does this do?
-sudo uwsgi --socket /var/run/uwsgi/uwsgi.sock --plugin /usr/lib/uwsgi/plugins/python3 web:app
-
 # start nginx service
 service nginx start
+
+# TODO: What does this do?
+cd /http/web-control-interface/pybackend
+uwsgi --socket /var/run/uwsgi/uwsgi.sock --plugin /usr/lib/uwsgi/plugins/python3 web:app &
+
