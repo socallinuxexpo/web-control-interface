c153f95aabaf37164a5c36960f707fa0813052e0
Derek Nowicki <derek@knowikiapps.com>
2017-02-19 22:16:30 -0800

    derekn: working provisioning script. changed the name to be more descriptive

---
+++ vagrant-provision.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#install
+pip install uwsgi flask flask-restful
+
+sudo mkdir /var/run/uwsgi
+sudo chmod 755 /var/run/uwsgi
+sudo chmod 777 /var/log/uwsgi
+rm -f /etc/nginx/sites-enabled/default
+
+# create some symbolic links for nginx
+sudo ln -s /http/web-control-interface/system-configs/web-ctrl /etc/nginx/sites-enabled/web-ctrl
+sudo ln -s /http/web-control-interface/system-configs/web-ctrl.conf /etc/init/web-ctrl.conf
+
+# start nginx service
+service nginx start
+
+# start uwsgi
+sudo uwsgi --socket /var/run/uwsgi/uwsgi.sock --plugin /usr/lib/uwsgi/plugins/python3 -w web:app &
