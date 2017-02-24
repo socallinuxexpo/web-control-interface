d9b495f54b760bda4dd2f89662d9ff666754f7a3
Derek Nowicki <derek@knowikiapps.com>
2017-02-23 19:11:04 -0800

    derekn: these changes get uwsgi working on the vagrant vm

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 #install
-pip install uwsgi flask flask-restful
+pip install uwsgi flask flask-restful pyserial
 
 sudo mkdir /var/run/uwsgi
 sudo chmod 755 /var/run/uwsgi
 sudo chmod 777 /var/log/uwsgi
 rm -f /etc/nginx/sites-enabled/default
 
-# create some symbolic links for nginx
+# create some symbolic link for nginx
 sudo ln -s /http/web-control-interface/system-configs/web-ctrl /etc/nginx/sites-enabled/web-ctrl
-sudo ln -s /http/web-control-interface/system-configs/web-ctrl.conf /etc/init/web-ctrl.conf
 
 # start nginx service
 service nginx start
 
 # start uwsgi
-sudo uwsgi --socket /var/run/uwsgi/uwsgi.sock --plugin /usr/lib/uwsgi/plugins/python3 -w web:app --chmod-socket=666 &
+uwsgi --ini /http/web-control-interface/system-configs/uwsgi.ini &
